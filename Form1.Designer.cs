1b992bf604c05c1920982f100f3ed9af893eab0f
JonaMC <jona.martin15@gmail.com>
2021-08-30 20:53:24 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace Perceptron
+
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelDiv = new System.Windows.Forms.TableLayoutPanel();
+            this.panelArriba = new System.Windows.Forms.Panel();
+            this.lblEpoca = new System.Windows.Forms.Label();
+            this.Perceptron = new System.Windows.Forms.Button();
+            this.InicializarPesos = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.learningR = new System.Windows.Forms.NumericUpDown();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.panelAbajo = new System.Windows.Forms.Panel();
+            this.PanelDiv.SuspendLayout();
+            this.panelArriba.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.learningR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PanelDiv
+            // 
+            this.PanelDiv.ColumnCount = 1;
+            this.PanelDiv.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.PanelDiv.Controls.Add(this.panelArriba, 0, 0);
+            this.PanelDiv.Controls.Add(this.panelAbajo, 0, 1);
+            this.PanelDiv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelDiv.Location = new System.Drawing.Point(0, 0);
+            this.PanelDiv.Name = "PanelDiv";
+            this.PanelDiv.RowCount = 2;
+            this.PanelDiv.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.PanelDiv.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.PanelDiv.Size = new System.Drawing.Size(1064, 681);
+            this.PanelDiv.TabIndex = 0;
+            // 
+            // panelArriba
+            // 
+            this.panelArriba.Controls.Add(this.lblEpoca);
+            this.panelArriba.Controls.Add(this.Perceptron);
+            this.panelArriba.Controls.Add(this.InicializarPesos);
+            this.panelArriba.Controls.Add(this.numericUpDown1);
+            this.panelArriba.Controls.Add(this.label2);
+            this.panelArriba.Controls.Add(this.label1);
+            this.panelArriba.Controls.Add(this.learningR);
+            this.panelArriba.Controls.Add(this.picBox);
+            this.panelArriba.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelArriba.Location = new System.Drawing.Point(0, 0);
+            this.panelArriba.Margin = new System.Windows.Forms.Padding(0);
+            this.panelArriba.Name = "panelArriba";
+            this.panelArriba.Size = new System.Drawing.Size(1064, 476);
+            this.panelArriba.TabIndex = 0;
+            // 
+            // lblEpoca
+            // 
+            this.lblEpoca.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEpoca.AutoSize = true;
+            this.lblEpoca.Location = new System.Drawing.Point(765, 174);
+            this.lblEpoca.Name = "lblEpoca";
+            this.lblEpoca.Size = new System.Drawing.Size(87, 13);
+            this.lblEpoca.TabIndex = 15;
+            this.lblEpoca.Text = "Épocas Actual: x";
+            this.lblEpoca.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblEpoca.Visible = false;
+            // 
+            // Perceptron
+            // 
+            this.Perceptron.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Perceptron.Location = new System.Drawing.Point(866, 137);
+            this.Perceptron.Name = "Perceptron";
+            this.Perceptron.Size = new System.Drawing.Size(189, 30);
+            this.Perceptron.TabIndex = 14;
+            this.Perceptron.Text = "Perceptrón";
+            this.Perceptron.UseVisualStyleBackColor = true;
+            this.Perceptron.Click += new System.EventHandler(this.Perceptron_Click);
+            // 
+            // InicializarPesos
+            // 
+            this.InicializarPesos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.InicializarPesos.Location = new System.Drawing.Point(866, 91);
+            this.InicializarPesos.Name = "InicializarPesos";
+            this.InicializarPesos.Size = new System.Drawing.Size(189, 30);
+            this.InicializarPesos.TabIndex = 13;
+            this.InicializarPesos.Text = "Inicializar pesos";
+            this.InicializarPesos.UseVisualStyleBackColor = true;
+            this.InicializarPesos.Click += new System.EventHandler(this.InicializarPesos_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(961, 49);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(94, 20);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(866, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Épocas máximas:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(866, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Learning Rate:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // learningR
+            // 
+            this.learningR.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.learningR.DecimalPlaces = 1;
+            this.learningR.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.learningR.Location = new System.Drawing.Point(961, 9);
+            this.learningR.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            65536});
+            this.learningR.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.learningR.Name = "learningR";
+            this.learningR.Size = new System.Drawing.Size(94, 20);
+            this.learningR.TabIndex = 9;
+            this.learningR.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.learningR.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            // 
+            // picBox
+            // 
+            this.picBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.picBox.Location = new System.Drawing.Point(9, 9);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(750, 460);
+            this.picBox.TabIndex = 8;
+            this.picBox.TabStop = false;
+            this.picBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.picBox_MouseClick);
+            // 
+            // panelAbajo
+            // 
+            this.panelAbajo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelAbajo.Location = new System.Drawing.Point(0, 476);
+            this.panelAbajo.Margin = new System.Windows.Forms.Padding(0);
+            this.panelAbajo.Name = "panelAbajo";
+            this.panelAbajo.Size = new System.Drawing.Size(1064, 205);
+            this.panelAbajo.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.PanelDiv);
+            this.MaximumSize = new System.Drawing.Size(1080, 720);
+            this.MinimumSize = new System.Drawing.Size(1080, 720);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Perceptrón";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PanelDiv.ResumeLayout(false);
+            this.panelArriba.ResumeLayout(false);
+            this.panelArriba.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.learningR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel PanelDiv;
+        private System.Windows.Forms.Panel panelArriba;
+        private System.Windows.Forms.Label lblEpoca;
+        private System.Windows.Forms.Button Perceptron;
+        private System.Windows.Forms.Button InicializarPesos;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown learningR;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Panel panelAbajo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Perceptron
+{
+    public partial class Form1 : Form
+    {
+        public struct Coords
+        {
+            public float x, y;
+            
+
+            public Coords(float _x, float _y)
+            {
+                x = _x;
+                y = _y;
+            }
+        }
+        Graphics grafico;
+        Bitmap btmp;
+        float escalaX, escalaY;
+        Coords pesos;
+        List<Coords> Clase1, Clase2;
+        Random r;
+
+        public Form1()
+        {   
+            InitializeComponent();
+            CargarGrafico();
+            Clase1 = new List<Coords>();
+            Clase2 = new List<Coords>();
+            r = new Random(new Random(846213213).Next());
+        }
+
+        private void Perceptron_Click(object sender, EventArgs e)
+        {
+            lblEpoca.Visible = true;
+        }
+
+        private void InicializarPesos_Click(object sender, EventArgs e)
+        {
+            float temp = (float)r.NextDouble() + (float)r.Next(0, 6);
+            if (r.Next(0, 2) % 2 != 0
+                ) temp = -temp;
+            pesos.x = temp;
+            temp = (float)r.NextDouble() + (float)r.Next(0, 6);
+            if (r.Next(0, 2) % 2 != 0) temp = -temp;
+            pesos.y = temp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            int radio = 10;
+            Coords p = new Coords((e.X - (picBox.Width >> 1)) / escalaX, ((e.Y / escalaY) - 5) * -1);
+            if (e.Button.Equals(MouseButtons.Right))
+            {
+                Clase1.Add(p);
+                grafico.FillRectangle(Brushes.Blue, e.X - radio, e.Y - radio, radio << 1, radio << 1);
+            }
+            else
+            {
+                Clase2.Add(p);
+                grafico.FillEllipse(Brushes.Red, e.X - radio, e.Y - radio, radio << 1, radio << 1);
+            }
+            picBox.Refresh();
+        }
+        
+        public void CargarGrafico()
+        {
+            btmp = new Bitmap(picBox.Width, picBox.Height);
+            grafico = Graphics.FromImage(btmp);
+            grafico.Clear(Color.White);
+            //dibuja el eje
+            grafico.DrawLine(new Pen(Color.Black), picBox.Width >> 1, 0, picBox.Width >> 1, picBox.Height);
+            grafico.DrawLine(new Pen(Color.Black), 0, picBox.Height >> 1, picBox.Width, picBox.Height >> 1);
+            
+            picBox.Image = btmp;
+
+            //dibujar la escala
+            escalaX = picBox.Width / 10;
+            escalaY = picBox.Height / 10;
+
+            for (int i = 0; i < 10; ++i)
+            {
+                grafico.DrawLine(new Pen(Color.Blue), escalaX * i, (picBox.Height >> 1) -5, escalaX * i, (picBox.Height >> 1)+5);
+                grafico.DrawLine(new Pen(Color.Blue), (picBox.Width >> 1)-5, escalaY * i, (picBox.Width >> 1)+5, escalaY * i);
+                //dibujar las etiquetas
+                grafico.DrawString("" + (i - 5), new Font("Arial", 10), new SolidBrush(Color.Black), escalaX * i, picBox.Height >> 1);
+                grafico.DrawString("" + (i - 5)*-1, new Font("Arial", 10), new SolidBrush(Color.Black), picBox.Width >> 1, escalaY * i);
+            }
+        }
+    }
+}
